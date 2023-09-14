bf9cfec3cacd7092a4b6aea0cee8d3fffa1b8484
mabp23rus@mail.ru <Sasha Wai@Home-PC>
2023-09-14 19:16:37 +0300

    Add recipes for blocks

--- src/main/java/net/sashawai/awesomeextracoal/AwesomeExtraCoalMod.java
+++ src/main/java/net/sashawai/awesomeextracoal/AwesomeExtraCoalMod.java
@@ -45,7 +45,7 @@ import java.util.function.Supplier;
 @Mod(modid = AwesomeExtraCoalMod.MODID, version = AwesomeExtraCoalMod.VERSION)
 public class AwesomeExtraCoalMod {
 	public static final String MODID = "awesome_extra_coal";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.0.1";
 	public static final SimpleNetworkWrapper PACKET_HANDLER = NetworkRegistry.INSTANCE.newSimpleChannel("awesome_extra_coal:a");
 	@SidedProxy(clientSide = "net.sashawai.awesomeextracoal.ClientProxyAwesomeExtraCoalMod", serverSide = "net.sashawai.awesomeextracoal.ServerProxyAwesomeExtraCoalMod")
 	public static IProxyAwesomeExtraCoalMod proxy;
